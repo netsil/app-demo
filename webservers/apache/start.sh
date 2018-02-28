eb732db8fd24883680e2e3aa0954d69ea1861916
luhkevin <kevin@netsil.com>
2018-02-28 22:26:14 +0000

    apache paths

--- webservers/apache/start.sh
+++ webservers/apache/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cp /usr/local/apache2/htdocs/index.html /usr/local/apache2/htdocs/myapache.html
+cp /usr/local/apache2/htdocs/index.html /usr/local/apache2/htdocs/apacheweb.html
 httpd
 /root/make-web-app-requests.sh
